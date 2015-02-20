0f662d27ae12193c57dfacc5e48243796058742e
Andrea Sassu <andrea.sassu@colorado.edu>
2015-02-19 23:51:22 -0700

    Basic functionality working

--- build/js/my/events.js
+++ build/js/my/events.js
@@ -3,5 +3,28 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    console.log('listing Sassu events')    
+    
+    $.get("https://api.github.com/orgs/asassu/events", github, function(data) {
+
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+
+        // Q: Why is JSON.parse no longer necessary?
+        var events = data
+        
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            repoView(events[0].full_name)
+
+        })
+
+    })
 }
